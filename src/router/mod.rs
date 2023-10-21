6e2e0f77697d04bd8bd221ddd80799d9526c1495
glendc <glen.decauwsemaecker@otainsight.com>
2023-10-22 00:42:33 +0200

    add sitemap/robots files

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,4 +1,6 @@
 use axum::{
+    http::HeaderMap,
+    response::IntoResponse,
     routing::{get, post},
     Router,
 };
@@ -9,9 +11,31 @@ mod redirect;
 
 pub mod not_found;
 
+pub async fn get_robots_txt() -> &'static str {
+    r"User-agent: *
+Allow: /
+Sitemap: https://bckt.xyz/sitemap.xml
+"
+}
+
+pub async fn get_sitemap_xml() -> impl IntoResponse {
+    let body = r#"<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://bckt.xyz/</loc>
+    </url>
+</urlset>"#;
+
+    let mut headers = HeaderMap::new();
+    headers.insert("content-type", "application/xml".parse().unwrap());
+
+    (headers, body)
+}
+
 pub fn new() -> Router {
     Router::new()
         .route("/", get(index::get))
+        .route("/robots.txt", get(get_robots_txt))
+        .route("/sitemap.xml", get(get_sitemap_xml))
         .route("/link", get(link::get))
         .route("/link", post(link::post))
         .route("/:hash", get(redirect::get))
