49aca24362c921e30806d51d89c95f8cca54906a
glendc <glen.decauwsemaecker@otainsight.com>
2023-10-22 01:31:03 +0200

    improve styling error/info boxes

--- src/auth.rs
+++ src/auth.rs
@@ -1,6 +1,10 @@
 #[derive(Debug)]
 pub struct Auth;
 
+// TODO implement using
+// - orion for encryption
+// - reqwest to send magic link over sendgrid api v3
+
 impl Auth {
     pub fn new(_private_key: String, _raw_auth_emails: String, _sendgrid_api_key: String) -> Auth {
         Auth
