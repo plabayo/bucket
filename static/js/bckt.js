e166a9fb2455fb1ad43dc8adeaabad350ceb3f4b
glendc <glen.decauwsemaecker@otainsight.com>
2023-10-22 22:07:37 +0200

    drop console logs from bckt.js

--- static/js/bckt.js
+++ static/js/bckt.js
@@ -1,8 +1,5 @@
 function init() {
-    console.log('hello');
-
     document.body.addEventListener('htmx:beforeSwap', (evt) => {
-        console.log('beforeSwap', evt);
         if ([400, 404].includes(evt.detail.xhr.status)) {
             evt.detail.shouldSwap = true;
             evt.detail.isError = false;
