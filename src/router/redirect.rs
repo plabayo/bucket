b333980f9fedebe67d44f41e053c52fc215c7661
glendc <glen.decauwsemaecker@otainsight.com>
2023-10-22 14:30:30 +0200

    remove all external links from src code

--- src/router/redirect.rs
+++ src/router/redirect.rs
@@ -1,5 +1,12 @@
 use axum::{extract::Path, response::Redirect};
 
 pub async fn get(Path(hash): Path<String>) -> Redirect {
-    Redirect::temporary(&format!("https://{hash}"))
+    match hash.as_str() {
+        "code" => Redirect::permanent("https://github.com/plabayo/bucket"),
+        "author" => Redirect::permanent("https://plabayo.tech"),
+        "og-image" => Redirect::permanent(
+            "https://upload.wikimedia.org/wikipedia/commons/3/3b/Sand_bucket.jpg",
+        ),
+        hash => Redirect::temporary(&format!("https://{hash}")),
+    }
 }
