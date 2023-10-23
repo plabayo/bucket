9d5085ef4556fff4e23ba42bfb9d699cbada60fd
glendc <glen.decauwsemaecker@otainsight.com>
2023-10-23 11:08:02 +0200

    add link support (shortlinks)
    TODO: actually store links in shared db

--- src/router/link.rs
+++ src/router/link.rs
@@ -1,34 +1,60 @@
+use std::sync::Arc;
+
 use askama::Template;
-use axum::Form;
+use askama_axum::{IntoResponse, Response};
+use axum::{extract::State, http::StatusCode, response::Redirect, Form};
 use serde::Deserialize;
+use tower_cookies::Cookies;
 
 #[derive(Template)]
-#[template(path = "../templates/index.html")]
+#[template(path = "../templates/content/link.html")]
 pub struct GetTemplate {
     pub email: String,
 }
 
-pub async fn get() -> GetTemplate {
-    GetTemplate {
-        email: "foo@example.com".to_string(),
+pub async fn get(State(state): State<Arc<crate::router::State>>, cookies: Cookies) -> Response {
+    if let Some(cookie) = cookies.get(crate::services::COOKIE_NAME) {
+        if let Some(email) = state.auth.verify_cookie(cookie.value()) {
+            return GetTemplate { email }.into_response();
+        }
     }
+    Redirect::temporary("/").into_response()
 }
 
 #[derive(Template)]
-#[template(path = "../templates/fragments/link_ok.html")]
-pub struct PostTemplate {
-    long: String,
-    short: String,
+#[template(path = "../templates/content/link_ok.html")]
+pub struct PostOkTemplate {
+    pub email: String,
+    pub long: String,
+    pub short: String,
 }
 
 #[derive(Deserialize)]
 pub struct PostParams {
-    link: String,
+    long: String,
 }
 
-pub async fn post(Form(params): Form<PostParams>) -> PostTemplate {
-    PostTemplate {
-        long: params.link,
-        short: "/abc123".to_string(),
+pub async fn post(
+    State(state): State<Arc<crate::router::State>>,
+    cookies: Cookies,
+    Form(params): Form<PostParams>,
+) -> Response {
+    if let Some(cookie) = cookies.get(crate::services::COOKIE_NAME) {
+        if let Some(email) = state.auth.verify_cookie(cookie.value()) {
+            return PostOkTemplate {
+                email,
+                long: params.long,
+                short: "example.com".to_string(),
+            }
+            .into_response();
+        }
     }
+    (
+        StatusCode::FORBIDDEN,
+        super::shared::ErrorTemplate {
+            title: "action forbidden".to_string(),
+            message: "You are not authorized for creating shortlinks.".to_string(),
+        },
+    )
+        .into_response()
 }
